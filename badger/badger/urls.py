d18ad050602dbe4fc506ec2a16812bd290190040
Bernardo Heynemann <heynemann@gmail.com>
2012-08-18 14:09:25 -0300

    Profile page halfway

--- badger/badger/urls.py
+++ badger/badger/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
-from badger.views import IndexView
+from badger.views import IndexView, ProfileView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +9,7 @@ from badger.views import IndexView
 
 urlpatterns = patterns('',
     (r'^$', IndexView.as_view()),
+    (r'^profile/?$', ProfileView.as_view()),
 
     url(r'^auth/', include('social_auth.urls')),
 
--- badger/badger/views.py
+++ badger/badger/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "badger/index.html"
+
+class ProfileView(TemplateView):
+    template_name = "badger/profile.html"
