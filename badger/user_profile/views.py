7eb978c03605b05170f44b383189a7d04401a64d
Rafael Caricio <rafael.jacinto@gmail.com>
2012-08-18 20:00:17 -0300

    dodnod

--- badger/user_profile/views.py
+++ badger/user_profile/views.py
@@ -5,4 +5,4 @@ class ProfileDetailView(DetailView):
     template_name = "profile/detail.html"
 
     def get_object(self):
-        return BadgerProfile.objects.filter(user__id=self.kwargs['pk'])[0]
+        return BadgerProfile.objects.filter(user__id=self.kwargs['pk'])
