a0e394c9374f7ff306a61a947d0e952191fb873a
Rafael Caricio <rafael.jacinto@gmail.com>
2012-08-18 10:29:02 -0300

    bubu.

--- badger/badger/settings.py
+++ badger/badger/settings.py
@@ -149,3 +149,11 @@ LOGGING = {
         },
     }
 }
+
+# other apps configurations
+
+GITHUB_APP_ID = 'ef2cb7e497e8e248b19d'
+GITHUB_API_SECRET = 'ef1dfbd6f8ede9fd2697533be062842d53561f63'
+
+GITHUB_EXTENDED_PERMISSIONS = []
+
